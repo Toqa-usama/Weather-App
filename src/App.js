5db7a9baca06d99da4cf198e836d2f793d7dd665
Toqa-usama <toqausama2003@gmail.com>
2024-02-08 08:59:06 +0200

    Create React App

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import Form from "./Component/Form";
+import Weather from "./Component/Weather";
+const API_KEY = "6be2257a17787018d08291bbea3c5d1d";
+class App extends Component {
+  state = {
+    tempreature: "",
+    city: "",
+    country: "",
+    humidity: "",
+    description: "",
+    error: "",
+  };
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}%2C${country}&appid=e36ed364400282e43250b6c4c0274d44`
+    );
+    const data = await api.json();
+
+    if (city && country) {
+      this.setState({
+        tempreature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.name.humidity,
+        description: data.weather[0].description,
+        error: "",
+      });
+    } else {
+      this.setState({
+        tempreature: "",
+        city: "",
+        country: "",
+        humidity: "",
+        description: "",
+        error: "Please Enter Data",
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="form-container">
+          <Form getWeather={this.getWeather} />
+          <Weather
+            tempreature={this.state.tempreature}
+            city={this.state.city}
+            country={this.state.country}
+            humidity={this.state.humidity}
+            description={this.state.description}
+            error={this.state.error}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
